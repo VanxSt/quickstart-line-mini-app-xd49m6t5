d7522f3c7c6e4f260798fb6ab33da922c527b81c
To-do-lish <noperfecf@gmail.com>
2026-08-29 20:19:11 +0700

    refactor: remove git log execution logic from vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-import { execSync } from 'child_process';
-import { writeFileSync } from 'fs';
-try {
-  const log = execSync('git log -p -n 5 google.gs', { encoding: 'utf8' });
-  writeFileSync('google-gs-log.txt', log);
-} catch (e) {
-  writeFileSync('google-gs-log.txt', `ERROR: ${e.message}`);
-}
-
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
